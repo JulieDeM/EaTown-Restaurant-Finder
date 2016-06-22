0d94b0b821fdcb365b3ff5e4fc572ef7b4fc8bf7
Julie DeMasters <juliekdemasters@gmail.com>
2016-06-22 11:51:32 -0600

    Add files via upload

--- sonoinfoislost.js
+++ sonoinfoislost.js
@@ -1,3 +1,6 @@
+//this is for page 2
+
+
 //****second api**************
 
 var data = null;
@@ -10,25 +13,21 @@ var button = document.getElementById('button');
 var newP = document.createElement('p');
 var newJ = document.createElement('p');
 var newK = document.createElement('p');
+var rest1 = Math.floor(Math.random(emptyArray)*4);
+var rest2 = Math.floor(Math.random(emptyArray)*4);
+var rest33 = Math.floor(Math.random(emptyArray)*4);
 var emptyArray = [];
-var picsAr = [];
 
 // xhr.withCredentials = true;
 
 xhr.addEventListener("readystatechange", function () {
 
   if (this.readyState === 4 && xhr.status < 400){
-    var rest1 = Math.floor(Math.random(emptyArray)*20);
-    console.log(rest1)
-    var rest2 = Math.floor(Math.random(emptyArray)*20);
-    console.log(rest2)
-    var rest3 = Math.floor(Math.random(emptyArray)*20);
-    console.log(rest3)
     // console.log(this.responseText);
     var places = JSON.parse(xhr.responseText);
-    var pic1 = Math.floor(Math.random(picsAr)*4);
-    var pic2 = Math.floor(Math.random(picsAr)*4);
-    var pic3 = Math.floor(Math.random(picsAr)*4);
+    var pic1 = Math.floor(Math.random(emptyArray)*4);
+    var pic2 = Math.floor(Math.random(emptyArray)*4);
+    var pic3 = Math.floor(Math.random(emptyArray)*4);
     // newP.innerHTML = places.restaurants[i].restaurant.location.city;
     //       one.appendChild(newP);
     console.table(places);
@@ -40,10 +39,10 @@ xhr.addEventListener("readystatechange", function () {
       newK.innerHTML = "";
       console.log(places.length * Math.floor(Math.random()));
         // if(searchValue === places.restaurants[pic1].restaurant.location.city){
-            newP.innerHTML = places.restaurants[rest1].restaurant.name;
+            newP.innerHTML = places.restaurants[places.length * Math.floor(Math.random())].restaurant.name;
             console.log(newP.innerHTML);
-            newJ.innerHTML = places.restaurants[rest2].restaurant.name;
-            newK.innerHTML = places.restaurants[rest3].restaurant.name;
+            newJ.innerHTML = places.restaurants[places.restaurants.length * Math.floor(Math.random())].restaurant.name;
+            newK.innerHTML = places.restaurants[places.restaurants.length * Math.floor(Math.random())].restaurant.name;
             one.appendChild(newP);
             two.appendChild(newJ);
             three.appendChild(newK);
@@ -61,7 +60,7 @@ xhr.setRequestHeader("postman-token", "a974c264-4d82-dad3-c68c-7755f8c5ab87");
 
 xhr.send(data);
 
-//had to use jquery for this one or wouldn't work
+//had to use jquery for first one or wouldn't work
 var settings = {
   "async": true,
   "crossDomain": true,
